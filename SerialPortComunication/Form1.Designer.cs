202b4c4fe6196e5021796b78eabba91a427d6de3
XiaoXiangJianKe <1416038523@qq.com>
2019-11-14 09:41:33 +0800

    Signed-off-by: XiaoXiangJianKe <1416038523@qq.com>

---
+++ SerialPortComunication/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SerialportComunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonOpenSerial = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxReceive = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "串口：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(73, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(73, 76);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "波特率：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonOpenSerial);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(14, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 125);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口设置";
+            // 
+            // buttonOpenSerial
+            // 
+            this.buttonOpenSerial.Location = new System.Drawing.Point(281, 73);
+            this.buttonOpenSerial.Name = "buttonOpenSerial";
+            this.buttonOpenSerial.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenSerial.TabIndex = 7;
+            this.buttonOpenSerial.Text = "打开串口";
+            this.buttonOpenSerial.UseVisualStyleBackColor = true;
+            this.buttonOpenSerial.Click += new System.EventHandler(this.buttonOpenSerial_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "串口设置";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonSend);
+            this.groupBox2.Controls.Add(this.textBoxSend);
+            this.groupBox2.Location = new System.Drawing.Point(14, 177);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(387, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "发送数据";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(281, 47);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 8;
+            this.buttonSend.Text = "发送数据";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(18, 47);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(220, 21);
+            this.textBoxSend.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxReceive);
+            this.groupBox3.Location = new System.Drawing.Point(14, 311);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(387, 116);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "接收数据";
+            // 
+            // textBoxReceive
+            // 
+            this.textBoxReceive.Location = new System.Drawing.Point(18, 32);
+            this.textBoxReceive.Multiline = true;
+            this.textBoxReceive.Name = "textBoxReceive";
+            this.textBoxReceive.ReadOnly = true;
+            this.textBoxReceive.Size = new System.Drawing.Size(338, 62);
+            this.textBoxReceive.TabIndex = 0;
+            this.textBoxReceive.TextChanged += new System.EventHandler(this.textBoxReceive_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 455);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonOpenSerial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxReceive;
+    }
+}
+
---
+++ SerialPortComunication/Form1.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialportComunication
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort = new SerialPort();
+        private bool isSend = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            comboBox1.Items.Clear();
+            foreach (string name in portNames)
+            {
+                comboBox1.Items.Add(name);
+            }
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+            comboBox2.Text = "115200";
+            serialPort.DataReceived += textBoxReceive_TextChanged;
+        }
+
+        private void buttonOpenSerial_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort.BaudRate = Convert.ToInt32(comboBox2.Text, 10);
+                    serialPort.Open();
+
+                    buttonOpenSerial.Text = "关闭串口";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                serialPort.Close();
+                buttonOpenSerial.Text = "打开串口";
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                if (textBoxSend.Text.Trim() != null)
+                {
+                    //serialPort.Write(textBoxSend.Text);
+
+                    byte[] buffer = Encoding.UTF8.GetBytes(textBoxSend.Text);
+                    serialPort.Write(buffer, 0, buffer.Length);
+
+                    //MessageBox.Show("发送数据" + textBoxSend.Text);
+                    isSend = true;
+                    Console.WriteLine("isSend=" + isSend);
+                }
+                else
+                {
+                    MessageBox.Show("发送数据为空！");
+                }
+            }
+        }
+
+        private void textBoxReceive_TextChanged(object sender, EventArgs e)
+        {
+            //if(isSend)
+            //{
+            //Thread.Sleep(1000);
+                try 
+                {
+                    Console.WriteLine(isSend);
+                    //string data = serialPort.ReadExisting();
+
+                    byte[] buffer = new byte[serialPort.BytesToRead];
+                    int length = serialPort.Read(buffer, 0, buffer.Length);
+                    string data = Encoding.UTF8.GetString(buffer, 0, length);
+
+                    //MessageBox.Show("接收数据" + data);
+                    if (data.Trim() != null)
+                    {
+                        textBoxReceive.AppendText(data);
+                        textBoxReceive.AppendText("\r\n");
+                    }
+                    isSend = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            //}
+
+        }
+
+
+    }
+}
